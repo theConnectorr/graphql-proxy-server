a1fa87836857064ad14195f52e72d421b9f11872
theworstkhoi <kylekhoitv@gmail.com>
2024-08-30 11:58:01 +0700

    fix: test

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const globalRouter = require("./routes/global");
-app.use("api/global", globalRouter);
+app.use("/api/global", globalRouter);
 
 const userRouter = require("./routes/user");
-app.use("api/user", userRouter);
+app.use("/api/user", userRouter);
 
 app.use(express.static("./public"));
 
